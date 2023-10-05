92560fee9e8c45c71f6c4cf047334aba35f21ab1
Stasik-Mom <118301816+Stasik-Mom@users.noreply.github.com>
2023-10-05 12:43:13 +0300

    Able to build and use the wrapper offline

--- packages/app/src/components/home/HomePage.tsx
+++ packages/app/src/components/home/HomePage.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 
 export const HomePage = () => (
   /* We will shortly compose a pretty homepage here. */
-  <h1>Welcome to Backstage!!ssssss!!!!</h1>
+  <h1>Welcome to Backstage!!ss11111ssss!!!!</h1>
 );
